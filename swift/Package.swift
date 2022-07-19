76139940c9d8e37d3a457251a6a951a158056960
chiosin <chiosin@163.com>
2022-07-19 15:26:11 +0800

    environment

--- swift/Package.swift
+++ swift/Package.swift
@@ -11,6 +11,9 @@ let package = Package(
         ],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
+            .package(url: "https://github.com/alibabacloud-sdk-swift/tea-utils.git", from: "0.1.0"),
+            .package(url: "https://github.com/aliyun/tea-swift.git", from: "0.3.0"),
+            .package(url: "https://github.com/apple/swift-system.git", from:"1.2.1"),
         ],
         targets: [
             .target(
--- swift/Sources/Darabonba_Env/Client.swift
+++ swift/Sources/Darabonba_Env/Client.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 open class Client {
-    public static func getEnv(_ key: String) -> String {
+    public static func getEnv(_ key: String) -> String? {
+        guard let rawValue = getenv(key) else { return nil }
+            return String(utf8String: rawValue)
     }
 
-    public static func setEnv(_ key: String, _ value: String) -> Void {
+    public static func setEnv(_ key: String, _ value: String,overwrite: Bool) -> Void {
+           setenv(key, value, overwrite ? 1 : 0)
     }
 }
